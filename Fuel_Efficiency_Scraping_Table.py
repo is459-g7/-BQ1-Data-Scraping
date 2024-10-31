a126fdbd0605ede12251099a8b228c1da9e67488
ckyjet <115978471+ckyjet@users.noreply.github.com>
2024-10-31 21:00:18 +0800

    Update Fuel_Efficiency_Scraping_Table.py

--- Fuel_Efficiency_Scraping_Table.py
+++ Fuel_Efficiency_Scraping_Table.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #!pip install pandas
-
-
-# In[2]:
-
 
 import pandas as pd
 
@@ -19,7 +9,7 @@ url = "https://en.wikipedia.org/wiki/Fuel_economy_in_aircraft"
 tables = pd.read_html(url)
 
 # Select and save tables by indexing or by examining each table's content
-# For this example, let's assume tables are in order of appearance
+
 tables_to_save = {"Commuter_flights.csv": tables[1], 
                   "Regional_Flights.csv": tables[2],
                   "Short_Haul_Flights.csv": tables[3],
@@ -28,12 +18,6 @@ tables_to_save = {"Commuter_flights.csv": tables[1],
 for filename, table in tables_to_save.items():
     table.to_csv(filename, index=False)
     print(f"Saved {filename}")
-
-    
-
-
-# In[ ]:
-
 
 
 
